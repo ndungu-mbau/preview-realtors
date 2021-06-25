78200fe392fdc1fee99f5398fc95bcc594776db7
Nelson Mbau <mbau.ndungu@gmail.com>
2021-06-25 10:43:47 +0300

    feat: Add connection module to use with adminbro

---
+++ src/utils/connection.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('Database connected')
+})
+
+mongoose.set('returnOriginal', true)
+
+export default db
